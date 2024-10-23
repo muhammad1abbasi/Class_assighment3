d872d8cf034cc110ac29510653da93cccf4c9910
muhammad1abbasi <159998253+muhammad1abbasi@users.noreply.github.com>
2024-10-23 16:06:37 +0500

    Update page.tsx

--- app/components/footer/page.tsx
+++ app/components/footer/page.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
-            <h1 className="text-2xl font-bold font-serif">Muhammad's </h1>
+            <h1 className="text-2xl font-bold font-serif">Muhammad </h1>
           </div>
 
           <div className="flex space-x-4 mb-4 md:mb-0">
